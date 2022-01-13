e8c69b1eb536cbe13c4091ec4bba35053673aeb8
Pedro Henrique <pedroalmeidavrb@gmail.com>
2022-01-12 21:51:43 -0300

    Começando a criar arquivo com as inserções de valores

---
+++ insert_tables.sql
@@ -0,0 +1,59 @@
+INSERT INTO Usuario
+VALUES 
+(
+   1,
+   "Alan Turing",
+   "alanturing@gmail.com",
+   "paidacomputacao"
+),
+
+(
+  2,
+   "Seu Sirigueijo",
+   "sirigueijog@gmail.com",
+   "maodevaca"
+),
+
+(
+  3,
+   "Lula Molusco",
+   "moluscog@gmail.com",
+   "clarinete"
+),
+
+(
+  4,
+   "Bob Esponja",
+   "bob@gmail.com",
+   "calcaquadrada"
+),
+
+(
+  5,
+   "Patrick Estrela",
+   "patrickg@gmail.com",
+   "pedra"
+),
+
+(
+  6,
+   "Sandy",
+   "sandyg@gmail.com",
+   "esquilo"
+);
+
+INSERT INTO Universitario VALUES (
+    5
+),
+
+(
+    6
+);
+
+INSERT INTO Funcionario VALUES (
+    3
+),
+
+(
+    4
+);
