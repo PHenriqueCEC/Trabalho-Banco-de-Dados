25f60b1b684607680f24067989a681602ec8127f
Pedro Henrique <pedroalmeidavrb@gmail.com>
2022-01-13 22:17:23 -0300

    Inserindo mais dados na insert_table

--- insert_tables.sql
+++ insert_tables.sql
@@ -129,3 +129,125 @@ INSERT INTO Endereco VALUES (
 
 );
 
+INSERT INTO Restaurante VALUES (
+  3,
+  "Siri Cascudo"
+),
+
+(
+  4
+  "Tudo gostoso"
+);
+
+INSERT INTO Avaliacao VALUES (
+  5,
+  3,
+  "Lanche gostoso mas veio pouca batata"
+),
+
+(
+  6,
+  5,
+  "Lanche impecável"
+);
+
+INSERT INTO Categoria VALUES (
+  3,
+  "Lanches"
+),
+
+(
+  4,
+  "Comida brasileira"
+);
+
+INSERT INTO Prato VALUES (
+
+  3,
+  "Hamburguer de Siri",
+  20,
+  "Pao, Bife, Bacon, Calabresa e Batata",
+  "Melhor hamburguer de todos"
+),
+
+(
+  3,
+  "Hamburguer Tradicional",
+  10,
+  "Pao, Bife e Batata",
+  "Bom e barato"
+
+),
+
+(
+  3,
+  "Hamburguer de Bacon",
+  15,
+  "Pao, Bife Bacon Batata",
+  "Gostoso e super recheado"
+),
+
+(
+  4,
+  "Feijoada",
+  25,
+  "Feijão, linguiça e torresmo",
+  "Feijoada mais completa da cidade"
+),
+
+(
+  4,
+  "PF",
+  15,
+  "Arroz, Feijão, Carne e Salada",
+  "Melhor PF da cidade"
+);
+
+INSERT INTO Bebida VALUES (
+  3,
+  "COca cola",
+  5.00
+
+),
+
+(
+  3,
+  "Guaraná",
+  4.95
+),
+
+(
+  4,
+  "Suco de laranja",
+  5.00
+
+),
+
+(
+  4,
+  "Água",
+  2.00
+);
+
+INSERT INTO Sobremesa VALUES (
+  3,
+  "Pudim",
+  2.55
+),
+
+(
+  4,
+  "Pavê",
+  2.00
+),
+
+(
+  4,
+  "Pudim",
+  3.00
+);
+
+INSERT INTO Pedido VALUES (
+  
+)
+
