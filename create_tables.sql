0f23b50461efffe0d98c4bd916b85071ec0d0378
Pedro Henrique <pedroalmeidavrb@gmail.com>
2022-01-17 21:46:58 -0300

    Alterando script de criação

--- create_tables.sql
+++ create_tables.sql
@@ -1,4 +1,4 @@
--- CREATE DATABASE IfoodRU;
+CREATE DATABASE IfoodRU;
 USE IfoodRU;
 
 CREATE TABLE Usuario (
@@ -12,49 +12,55 @@ CREATE TABLE Usuario (
 
 CREATE TABLE Universitario (
     cod int AUTO_INCREMENT,
+    codUsuario int NOT NULL,
 
     PRIMARY KEY(cod),
-    FOREIGN KEY (cod) REFERENCES Usuario(cod)
+    FOREIGN KEY (codUsuario) REFERENCES Usuario(cod)
 
 );
 
 CREATE TABLE Funcionario (
     cod int AUTO_INCREMENT,
+    codUsuario int NOT NULL,
 
     PRIMARY KEY(cod),
-    FOREIGN KEY (cod) REFERENCES Usuario(cod)
+    FOREIGN KEY (codUsuario) REFERENCES Usuario(cod)
 
 );
 
 CREATE TABLE Motoboy (
     cod int AUTO_INCREMENT,
+    codUsuario int NOT NULL,
     cnh varchar(30) UNIQUE NOT NULL,
     placa varchar(10) UNIQUE NOT NULL,
 
     PRIMARY KEY(cod),
-    FOREIGN KEY (cod) REFERENCES Usuario(cod)
+    FOREIGN KEY (codUsuario) REFERENCES Usuario(cod)
 
 );
 
 CREATE TABLE Administrador (
     cod int AUTO_INCREMENT,
+    codUsuario int NOT NULL,
 
     PRIMARY KEY(cod),
-    FOREIGN KEY (cod) REFERENCES Usuario(cod)
+    FOREIGN KEY (codUsuario) REFERENCES Usuario(cod)
 
 );
 
 CREATE TABLE Sugestoes (
     id int AUTO_INCREMENT,
+    codUniversitario int NOT NULL,
     descricao varchar(256) NOT NULL,
 
      PRIMARY KEY(id),
-     FOREIGN KEY (id) REFERENCES Universitario(cod)
+     FOREIGN KEY (codUniversitario) REFERENCES Universitario(cod)
 
 );
 
 CREATE TABLE Endereco (
     cod int AUTO_INCREMENT,
+    codUniversitario int NOT NULL,
     cep varchar(10) NOT NULL,
     logradouro varchar(100) NOT NULL, 
     bairro varchar(30) NOT NULL,
@@ -64,86 +70,109 @@ CREATE TABLE Endereco (
     estado varchar(20) NOT NULL,
 
     PRIMARY KEY(cod),
-    FOREIGN KEY (cod) REFERENCES Universitario(cod)
+    FOREIGN KEY (codUniversitario) REFERENCES Universitario(cod)
 );
 
 CREATE TABLE Restaurante (
     id int AUTO_INCREMENT,
+    codFuncionario int NOT NULL,
     nome varchar(30) NOT NULL,
 
     PRIMARY KEY(id),
-    FOREIGN KEY (id) REFERENCES Funcionario(cod) 
+    FOREIGN KEY (codFuncionario) REFERENCES Funcionario(cod) 
 
 );
 
 CREATE TABLE Categoria (
     cod int AUTO_INCREMENT,
+    codRestaurante int NOT NULL,
     nome varchar(20) NOT NULL,
 
     PRIMARY KEY(cod),
-    FOREIGN KEY (cod) REFERENCES Restaurante(id)
+    FOREIGN KEY (codRestaurante) REFERENCES Restaurante(id)
+
+);
+
+CREATE TABLE CategoriaPrato (
+    cod int AUTO_INCREMENT,
+    nome varchar(20) NOT NULL,
+
+    PRIMARY KEY(cod)
 
 );
 
 CREATE TABLE Prato (
-    codPrato int AUTO_INCREMENT,
-    cod int NOT NULL,
+    cod int AUTO_INCREMENT,
+    codRestaurante int NOT NULL,
+    codCategoriaPrato int NOT NULL,
     nome varchar(20) NOT NULL,
     preco float NOT NULL,
     ingredientes varchar(100) NOT NULL,
     descricao varchar(100) NOT NULL,
  
-    PRIMARY KEY(codPrato),
-    FOREIGN KEY (cod) REFERENCES Restaurante(id)
+    PRIMARY KEY(cod),
+    FOREIGN KEY (codRestaurante) REFERENCES Restaurante(id),
+    FOREIGN KEY (codCategoriaPrato) REFERENCES CategoriaPrato(cod)
 
 );
 
 CREATE TABLE Bebida (
-    codBebida int AUTO_INCREMENT,
-    cod int NOT NULL,
+    cod int AUTO_INCREMENT,
+    codRestaurante int NOT NULL,
     nome varchar(20) NOT NULL,
     preco float NOT NULL,
 
-    PRIMARY KEY(codBebida),
-    FOREIGN KEY (cod) REFERENCES Restaurante(id)
+    PRIMARY KEY(cod),
+    FOREIGN KEY (codRestaurante) REFERENCES Restaurante(id)
 
 );
 
 CREATE TABLE Sobremesa (
-    codSobremesa int AUTO_INCREMENT,
-    cod int NOT NULL,
+    cod int AUTO_INCREMENT,
+    codRestaurante int NOT NULL,
     nome varchar(20) NOT NULL,
     preco float NOT NULL,
 
-    PRIMARY KEY(codSobremesa),
-    FOREIGN KEY (cod) REFERENCES Restaurante(id)
+    PRIMARY KEY(cod),
+    FOREIGN KEY (codRestaurante) REFERENCES Restaurante(id)
 
 );
 
 CREATE TABLE Pedido (
-    codPedido int AUTO_INCREMENT,
-    cod int NOT NULL,
+    cod int AUTO_INCREMENT,
+    codRestaurante int NOT NULL,
     statusPedido varchar(20) NOT NULL,
     previsaoEntrega TIME NOT NULL,
     origem varchar(50) NOT NULL,
     destino varchar(100) NOT NULL, 
 
-    PRIMARY KEY(codPedido),
-    FOREIGN KEY (cod) REFERENCES Restaurante(id)
+    PRIMARY KEY(cod),
+    FOREIGN KEY (codRestaurante) REFERENCES Restaurante(id)
 
 );
 
 CREATE TABLE Cupom (
-    codCupom int AUTO_INCREMENT,
-    cod int NOT NULL,
+    cod int AUTO_INCREMENT,
+    codRestaurante int NOT NULL,
     nome varchar(20) NOT NULL,
     descricao varchar(20) NOT NULL,
     valor float NOT NULL,
     statusCupom Boolean NOT NULL,
 
-    PRIMARY KEY(codCupom),
-    FOREIGN KEY (cod) REFERENCES Restaurante(id)
+    PRIMARY KEY(cod),
+    FOREIGN KEY (codRestaurante) REFERENCES Restaurante(id)
 
+);
+
+CREATE TABLE Avaliacao (
+    cod int AUTO_INCREMENT,
+    codUniversitario int NOT NULL,
+    nota int NOT NULL,
+    descricao varchar(200) NOT NULL,
+
+    PRIMARY KEY(cod),
+    FOREIGN KEY (codUniversitario) REFERENCES Universitario(cod)
+    
 );
 
 CREATE TABLE RestaurantePedidoAvaliacao (
@@ -156,25 +185,4 @@ CREATE TABLE RestaurantePedidoAvaliacao (
     FOREIGN KEY (codRestaurante) REFERENCES Restaurante(id),
     FOREIGN KEY (codPedido) REFERENCES Pedido(cod),
     FOREIGN KEY (codAvaliacao) REFERENCES Avaliacao(cod)
-);
-
-CREATE TABLE Avaliacao (
-    codAvaliacao int AUTO_INCREMENT,
-    cod int NOT NULL,
-    nota int NOT NULL,
-    descricao varchar(200) NOT NULL,
-
-    PRIMARY KEY(codAvaliacao),
-    FOREIGN KEY (cod) REFERENCES Universitario(cod)
-    
-);
-
-CREATE TABLE CategoriaPrato (
-    codCategoria int AUTO_INCREMENT,
-    cod int NOT NULL,
-    nome varchar(20) NOT NULL,
-
-    PRIMARY KEY(codCategoria),
-    FOREIGN KEY (cod) REFERENCES Prato(codPrato)
-
 );
